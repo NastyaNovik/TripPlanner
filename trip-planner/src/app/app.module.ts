8ab6fb351b6d49d20554924146078947e4786fbe
Anastasiya Novik <anastasiya.novik@madnesspartners.com>
2025-05-21 21:06:26 +0200

    introduce cards

--- trip-planner/src/app/app.module.ts
+++ trip-planner/src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TripNotesListComponent } from './components/trip-notes-list/trip-notes-list.component';
 import { TripNoteEditorComponent } from './components/trip-note-editor/trip-note-editor.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { NgOptimizedImage } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { TripCardComponent } from './components/trip-card/trip-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TripNotesListComponent,
     TripNoteEditorComponent,
-    NavbarComponent
+    NavbarComponent,
+    TripCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    NgOptimizedImage,
+    MatIconModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- trip-planner/src/app/components/trip-notes-list/trip-notes-list.component.ts
+++ trip-planner/src/app/components/trip-notes-list/trip-notes-list.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TripService} from '../../services/trip.service';
+import {Trip} from '../../models/trip';
 
 @Component({
   selector: 'app-trip-notes-list',
   standalone: false,
   templateUrl: './trip-notes-list.component.html',
-  styleUrl: './trip-notes-list.component.css'
+  styleUrl: './trip-notes-list.component.scss'
 })
-export class TripNotesListComponent {
 
+export class TripNotesListComponent implements OnInit {
+  upcomingTrips: Trip[] = [];
+  pastTrips: Trip[] = [];
+
+  constructor(private tripService: TripService) {}
+
+  ngOnInit() {
+    this.tripService.getTrips().subscribe(({ upcoming, past }) => {
+      this.upcomingTrips = upcoming.map(trip => ({
+        ...trip,
+        season: this.getSeason(trip.dateFrom)
+      }));
+
+      this.pastTrips = past.map(trip => ({
+        ...trip,
+        season: this.getSeason(trip.dateFrom)
+      }));
+    });
+  }
+
+  getSeason(dateFrom: string){
+    const month = parseInt(dateFrom.split('-')[1]);
+    switch(month){
+      case 12:
+      case 1:
+      case 2:
+          return 'winter';
+      case 3:
+      case 4:
+      case 5:
+        return 'spring';
+      case 6:
+      case 7:
+      case 8:
+        return 'summer';
+      case 9:
+      case 10:
+      case 11:
+        return 'autumn';
+      default:
+        return 'summer';
+    }
+  }
 }

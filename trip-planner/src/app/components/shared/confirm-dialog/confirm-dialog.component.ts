41e7e6b9144b70034e994c4c39daacf7f14135a9
Anastasiya Novik <anastasiya.novik@madnesspartners.com>
2025-07-20 23:17:05 +0200

    standalone component

--- trip-planner/src/app/components/shared/confirm-dialog/confirm-dialog.component.ts
+++ trip-planner/src/app/components/shared/confirm-dialog/confirm-dialog.component.ts
@@ -1,11 +1,13 @@
 import {Component, Inject} from '@angular/core';
-import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MAT_DIALOG_DATA, MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-confirm-dialog',
-  standalone: false,
+  standalone: true,
   templateUrl: './confirm-dialog.component.html',
-  styleUrl: './confirm-dialog.component.scss'
+  styleUrl: './confirm-dialog.component.scss',
+  imports: [MatDialogModule, MatButtonModule]
 })
 export class ConfirmDialogComponent {
   constructor(@Inject(MAT_DIALOG_DATA) public message: string) {}
